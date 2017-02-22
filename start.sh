8c870cb771eed8e45d4f9019fc4b2741d2fab0cd
Kila2 <277014717@qq.com>
2017-02-22 15:32:16 +0800

    init

---
+++ start.sh
@@ -0,0 +1,9 @@
+USER1=kila \
+USER1_PASSWORD=kila2l \
+APP_ID=appid \
+MASTER_KEY=masterkey \
+REST_API_KEY=restapikey \
+PARSE_DASHBOARD_ALLOW_INSECURE_HTTP=1 \
+SERVER_URL=http://localhost:1337/parse \
+GRAPHQL_SUPPORT=true \
+docker-compose up -d
