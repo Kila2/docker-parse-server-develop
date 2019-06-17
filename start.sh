bd5e2a3e7607652ca2aa05418595aa925a0d8707
ljl186502 <ljl186502@alibaba-inc.com>
2019-06-17 16:03:34 +0800

    update version

--- start.sh
+++ start.sh
@@ -1,13 +1,1 @@
-USER1=kila \
-USER1_PASSWORD=kila2l \
-APP_ID=appid \
-MASTER_KEY=masterkey \
-REST_API_KEY=restapikey \
-CLIENT_KEY=clientkey \
-ALLOW_CLIENT_CLASS_CREATION=1 \
-PARSE_DASHBOARD_ALLOW_INSECURE_HTTP=1 \
-SERVER_URL=http://localhost:1337/parse \
-LIVEQUERY_SUPPORT=true \
-LIVEQUERY_CLASSES=User,People \
-GRAPHQL_SUPPORT=true \
 docker-compose up -d
