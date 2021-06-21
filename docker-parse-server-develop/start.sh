192a5fff88fff70d13c622e8bf66d1503d891203
kila2 <lijunliang9819@qq.com>
2021-06-22 00:14:49 +0800

    update config

--- docker-parse-server-develop/start.sh
+++ docker-parse-server-develop/start.sh
@@ -1,1 +1,5 @@
+eval $(docker-machine env)
 docker-compose up -d
+sudo sed -i '' "s/192.168.99.101 parse//g" /etc/hosts
+sudo sed -i '' "/^$/d"  /etc/hosts
+echo '192.168.99.101 parse' | sudo tee -a /etc/hosts
